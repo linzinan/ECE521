8dce5db28d4cc743e3238dd470c8d77ec8ed1c4b
Gary Shin <gary@nanoleaf.me>
2018-01-31 20:12:34 -0500

    fixed issue

--- a1.py
+++ a1.py
@@ -81,7 +81,7 @@ def partition_data(Data, Target):
 	testData, testTarget = Data[randIdx[90:100]], Target[randIdx[90:100]]
 	return trainData, trainTarget, validData, validTarget, testData, testTarget
 
-def test_k_values(data_points, trainData, trainTarget, kval = None):
+def test_k_values(data_points, trainData, trainTarget, data_target):
 	k_list = [1, 3, 5, 50]
 	prediction_list = list()
 	data_mse = list()
@@ -93,7 +93,7 @@ def test_k_values(data_points, trainData, trainTarget, kval = None):
 			full_rank_resp.append(resp)
 
 		prediction = np.matmul(np.transpose(trainTarget), np.transpose(full_rank_resp))
-		mse = mean_squared_error(prediction, trainTarget)
+		mse = mean_squared_error(prediction, data_target)
 		prediction_list.append(prediction)
 		data_mse.append(mse)
 
@@ -176,30 +176,29 @@ def main():
 
 	#Train MSE
 	print ("Train MSE")
-	train_mse, prediction_list = test_k_values(trainData, trainData, trainTarget)
+	train_mse, prediction_list = test_k_values(trainData, trainData, trainTarget, trainTarget)
 	print (train_mse)
 	print (prediction_list)
 
-
-
 	#Validation MSE
 	print ("Validation MSE")
-	valid_mse, prediction_list = test_k_values(validData, trainData, trainTarget)
+	valid_mse, prediction_list = test_k_values(validData, trainData, trainTarget, validTarget)
 	print (valid_mse)
 	print (prediction_list)
 
 	#Test MSE
 	print ("Test MSE")
-	test_mse, prediction_list = test_k_values(testData, trainData, trainTarget)
+	test_mse, prediction_list = test_k_values(testData, trainData, trainTarget, testTarget)
 	print (test_mse)
 	print (prediction_list)
 
 	#random points MSE
 	print ("X MSE")
 	X = np.linspace(0.0, 11.0, num = 1000)[:, np.newaxis]
-	x_mse, prediction_list = test_k_values(X, trainData, trainTarget)
+	x_mse, prediction_list = test_k_values(X, trainData, trainTarget, trainTarget)
 	print (x_mse)
 	print (prediction_list)
+
 	quit()
 	# for prediction in prediction_list:
 	# 	plt.figure()
